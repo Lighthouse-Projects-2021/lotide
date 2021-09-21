267fbd0a412ab78be9535bf9cfc6d12f290e81b1
Kenia Balestra <kenia.balestra@gmail.com>
2021-09-21 02:51:33 +0000

    ES6 shorthand

--- index.js
+++ index.js
@@ -15,21 +15,41 @@ const middle = require('./middle');
 const findKey = require('./findKey');
 const findKeyByValue = require('./findKeyByValue');
 
+// ES6 shorthand property values
 module.exports = {
-    head:   head,
-    tail:   tail,
-    middle: middle,
-    eqArrays: eqArrays,
-    assertEqual: assertEqual,
-    assertArraysEqual: assertArraysEqual,
-    assertObjectsEqual: assertObjectsEqual,
-    eqObjects: eqObjects,
-    findKey: findKey,
-    findKeyByValue: findKeyByValue,
-    countLetters: countLetters,
-    countOnly: countOnly,
-    letterPositions: letterPositions,
-    map: map,
-    takeUntil: takeUntil,
-    without: without
-  };
+  head,
+  tail,
+  middle,
+  eqArrays,
+  assertEqual,
+  assertArraysEqual,
+  assertObjectsEqual,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  countLetters,
+  countOnly,
+  letterPositions,
+  map,
+  takeUntil,
+  without
+};
+
+// module.exports = {
+//     head:   head,
+//     tail:   tail,
+//     middle: middle,
+//     eqArrays: eqArrays,
+//     assertEqual: assertEqual,
+//     assertArraysEqual: assertArraysEqual,
+//     assertObjectsEqual: assertObjectsEqual,
+//     eqObjects: eqObjects,
+//     findKey: findKey,
+//     findKeyByValue: findKeyByValue,
+//     countLetters: countLetters,
+//     countOnly: countOnly,
+//     letterPositions: letterPositions,
+//     map: map,
+//     takeUntil: takeUntil,
+//     without: without
+//   };
